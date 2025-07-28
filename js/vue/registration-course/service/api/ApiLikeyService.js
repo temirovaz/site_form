574a8932ea6b0d6996188eb79b5f3c114b359266
Temirov Artur <temirovaz@likey.su>
2025-07-28 23:45:44 +0300

    Добавлен маппинг полей адресов для передачи в 1С: postal_address, postal_index, legal_address

--- js/vue/registration-course/service/api/ApiLikeyService.js
+++ js/vue/registration-course/service/api/ApiLikeyService.js
@@ -15,17 +15,41 @@ export default class ApiLikeyService {
             });
              return program;
         });
+        
         let dataForm = store.state.form
+        
+        // Обработка поля basis для 1С
         if(dataForm.payment?.basis !== undefined){
             dataForm.payment.footing = dataForm.payment.basis;
-            delete  dataForm.payment.basis
+            delete dataForm.payment.basis;
+        }
+        
+        // Маппинг полей адресов для 1С
+        if(dataForm.payment) {
+            // Маппинг фактического адреса
+            if(dataForm.payment.actual_address) {
+                dataForm.payment.postal_address = dataForm.payment.actual_address;
+                delete dataForm.payment.actual_address;
+            }
+            
+            // Маппинг почтового индекса
+            if(dataForm.payment.postal_code) {
+                dataForm.payment.postal_index = dataForm.payment.postal_code;
+                delete dataForm.payment.postal_code;
+            }
+            
+            // Маппинг юридического адреса
+            if(dataForm.payment.address_value) {
+                dataForm.payment.legal_address = dataForm.payment.address_value;
+                // Не удаляем address_value, так как он может использоваться в других местах
+            }
         }
         
        return api.likey.post('/CreateData/3_00', {
             type : 'application',
             algorithm: 2,
             programs: programs,
-            data: store.state.form
+            data: dataForm
         })
     }
 
