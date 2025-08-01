0d52ca1c14de33c7ce79f12c0d55db967d2f4451
Artur Temirov <temirovaz@likey.su>
2025-08-01 14:48:41 +0300

    Обновление contacts.vue в ветке main

--- js/vue/registration-course/components/wizard/steps/contacts.vue
+++ js/vue/registration-course/components/wizard/steps/contacts.vue
@@ -5,15 +5,21 @@
       <form>
         <FormField label="Телефон" name="phone" rules="required" v-model="phone"  placeholder="+7(___)___-__-__" />
         <SuggestionEmail label="Эл. почта" rules="required|email" v-model="email"></SuggestionEmail>
+        <div class="checkbox-container">
+          <input type="checkbox" id="privacy-policy" v-model="privacyPolicyAccepted" checked>
+          <label for="privacy-policy">
+            Я согласен с <a href="https://likey.su/include/licenses_detail.php" target="_blank">политикой обработки персональных данных</a>
+          </label>
+        </div>
+        <div v-if="showPrivacyPolicyError" class="error-message">
+          Необходимо принять политику
+        </div>
       </form>
     </ValidationObserver>
   </div>
-
 </template>
 
 <script>
-
-
 import DaDataSuggestion from "../../dadata-suggestion";
 import SuggestionEmail from "../../../components/suggestion/suggestion-email";
 
@@ -25,19 +31,37 @@ export default {
     return {
       phone: '',
       email: '',
+      privacyPolicyAccepted: true,
+      showPrivacyPolicyError: false,
     }
   },
   watch: {
-
     clickedNext: function(status) {
       if(status === true){
+        this.showPrivacyPolicyError = false;
         this.$refs.form.validate().then(success => {
-          this.$store.commit('saveDataFormForStep', {contact : {email: this.email, phone: this.phone}})
-          this.$emit('can-continue', {status: success})
+          if (success && this.privacyPolicyAccepted) {
+            this.$store.commit('saveDataFormForStep', {contact : {email: this.email, phone: this.phone}})
+            this.$emit('can-continue', {status: true})
+          } else if (!this.privacyPolicyAccepted) {
+            this.showPrivacyPolicyError = true;
+            this.$emit('can-continue', {status: false})
+          } else {
+            this.$emit('can-continue', {status: false})
+          }
         });
       }
     }
   }
-
 }
 </script>
+
+<style scoped>
+.checkbox-container {
+  margin-top: 15px;
+}
+.error-message {
+  color: red;
+  margin-top: 5px;
+}
+</style>
